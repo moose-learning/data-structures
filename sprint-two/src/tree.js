1ee087580e47fcc9705143f9d0a80eba6c9a03a6
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-27 02:13:05 -0700

    [Complete] Tree close #2

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,26 +1,49 @@
 var Tree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);;
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
-treeMethods.addChild = function(value){
-
+treeMethods.addChild = function(value){  
+  this.value = value;
+  this.children.push(new Tree(value));
 };
+
+function findValue(arr, target) {
+  if (arr && (arr === target)) {
+    return true;
+  }
+  
+  for (let i = 0; i < arr.length; i++) {
+    const curr = arr[i];
+    
+    if (curr.value === target) return true;
+    if (curr.children) {
+      findValue(curr.children, target);
+    }
+    
+  }
+  
+  return false;
+}
 
 treeMethods.contains = function(target){
-
+  if (this.value === target) return true;
+  
+  
+  if (this.children.length > 0) {
+    return findValue(this.children, target);
+  }
+    
+  return false;
 };
+
 
 
 /*

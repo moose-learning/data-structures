da8a86d1bf16d27a5e61b899145824f3b245014f
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-27 15:20:44 -0700

    [#3-linkedList] complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,16 +1,68 @@
+const searchValue = (node, target) => {  
+  if (node.value === target) {
+    return true;
+  }
+  
+  if (node.next) {
+    return searchValue(node.next, target);
+  }
+
+  return false;  
+};
+
 var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  list._length = 0;
 
   list.addToTail = function(value){
+    const node = new Node(value);
+    let currentNode = list.head;  
+    
+    if (!currentNode) {
+      list.head = node;
+      list.tail = node;
+      list.head.next = list.tail
+      list._length++;
+      
+      return
+    }
+    
+    if (list.head) {
+      list.tail.next = node;
+      list.tail = node;
+      list._length++;
+    }
+    
+    // console.log(list);
   };
 
   list.removeHead = function(){
+    // console.log(list);
+    const formerHead = Object.assign({}, list.head);
+    const nextHead = Object.assign({}, list.head.next);
+    
+    list.head = nextHead;
+    list._length--;
+    
+    return formerHead.value;
   };
 
   list.contains = function(target){
+    if (list.head && list.head.value === target) return true;
+    if (list.tail && list.tail.value === target) return true;
+    
+    if (list.head.next) {
+      return searchValue(list.head, target);
+    }
+    
+    return false;
   };
+  
+  list.getLength = function() {
+    return list._length;
+  }
 
   return list;
 };

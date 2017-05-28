8d57beacc1dacae50cea3e9edaa4108aa8239454
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-28 12:13:14 -0700

    [binarySearchTree-#8] complete insert method (#8)

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,30 @@
 var BinarySearchTree = function(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  const node = new BinarySearchTree(value);
+  
+  if (this.value > value) {
+    if (this.left !== null) this.left.insert(value);
+    else this.left = node;
+  }
+  
+  if (this.value < value) {
+    if (!this.right) this.right = node;
+    else this.right.insert(value);
+  } 
+}
+
+BinarySearchTree.prototype.contains = function(value) {
+  
+}
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+  
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?

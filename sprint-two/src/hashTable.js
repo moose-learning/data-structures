d0e53d04442bcdbc2aed77e0003615246a5d6e0a
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-27 17:57:43 -0700

    [hashTable-#7] (#7): completed all except sizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,39 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  
+  if (this._storage[i] && !Array.isArray(this._storage[i])) {
+    const tempKey = Object.keys(this._storage[i])[0];
+    const tempVal = this._storage[i][tempKey];
+    
+    this._storage[i] = [];
+    this._storage[i].push({ [tempKey]: tempVal });
+  }
+  
+  if (Array.isArray(this._storage[i])) {
+    this._storage[i][0][k] = v;
+  }
+  
+  if (!this._storage[i]) {
+    this._storage[i] = { [k]: v };
+  }
+  
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  if (Array.isArray(this._storage[i])) {
+    return this._storage[i][0][k];
+  } else if (this._storage[i] && !Array.isArray(this._storage[i])) {
+    return this._storage[i][k];    
+  }
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  
+  this._storage[i][k] = null;    
 
 };
 

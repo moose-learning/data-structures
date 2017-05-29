718c16a9bd1014f41c1a6fea018409ec04582188
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-29 02:42:50 -0700

    [stack-#9] functional stack complete #9

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,18 +3,28 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let index = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
-
+    index++;
+    storage[index] = value;
   };
 
   someInstance.pop = function(){
-
+    if (!storage[index]) return null;
+    
+    const poppedItem = storage[index];
+    
+    delete storage[index];
+    
+    index--;
+    
+    return poppedItem;
   };
 
   someInstance.size = function(){
-
+    return index;
   };
 
   return someInstance;

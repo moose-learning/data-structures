8dd48e5af47c1795902a23b3981fd107ff26af82
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-30 18:15:36 -0700

    [stack-#9] complete pseudoclassical #9

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.index = 0;
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value) {
+  this.index++;
+  this.storage[this.index] = value;  
+};
+
+Stack.prototype.pop = function() {  
+  const poppedProp = this.storage[this.index];  
+
+  delete this.storage[this.index];  
+  
+  this.index--;
+  
+  return poppedProp;
+};
+
+Stack.prototype.size = function() {
+  return this.index <= 0 ? 0 : this.index;
+}
+
 
 

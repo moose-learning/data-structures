a0cf061bec8c3d2cf406e06795e262890e772c99
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-30 18:14:35 -0700

    [stack-#9] complete stack pseudoclassical #9

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  
+  stack.storage = {};
+  stack.index = 0;  
+  
+  return stack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.index++;
+  this.storage[this.index] = value;  
+};
+
+stackMethods.pop = function() {  
+  const poppedProp = this.storage[this.index];  
+
+  delete this.storage[this.index];  
+  
+  this.index--;
+  
+  return poppedProp;
+};
+
+stackMethods.size = function() {
+  return this.index <= 0 ? 0 : this.index;
+}
 
 

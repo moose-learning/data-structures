111d130cd1d88798a2bc5418fda9eb1e76a8034c
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-31 22:19:05 -0700

    [queue-#10] complete functions #10

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,18 +3,37 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  const len = Object.keys(storage).length;
+  let index = 0;
 
   // Implement the methods below
   someInstance.enqueue = function(value){
-
+    index++;
+    storage[index] = value;
   };
 
   someInstance.dequeue = function(){
-
+    const len = Object.keys(storage).length;
+    const justBefore = len - 1;
+    const shiftedItem = storage[index - justBefore];
+    
+    if (!shiftedItem) return null;
+    
+    delete storage[index - len];
+    
+    
+    index--;
+    if (len > 1) {
+      storage[index] = storage[index + 1]      
+    }
+    
+    return shiftedItem;
   };
 
   someInstance.size = function(){
+    const len = Object.keys(storage).length;
 
+    return index;
   };
 
   return someInstance;
